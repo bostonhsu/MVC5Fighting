eed81a7412b5c16961cd34fec68e89bee4f23c2e
Boston Hsu <bostonhsu@outlook.com>
2017-07-13 10:48:47 +0800

    done

---
+++ MVC5Fighting/HttpHandlersDemo/Components/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HttpHandlersDemo.Components
+{
+    public class HomeController
+    {
+        public String Test(Object param1)
+        {
+            var message = "<html><h1>Got it! You passed '{0}' </h1></html>";
+            return string.Format(message, param1);
+        }
+    }
+}
