e4eccb711820518e68206cabc4de4b1fae133a01
Boston Hsu <bostonhsu@outlook.com>
2017-07-13 18:06:35 +0800

    重新做了一遍试验。

---
+++ MVC5Fighting/Sample1_1_1/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sample1_1_1.Models;
+
+namespace Sample1_1_1.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string SendHello(HelloPerson helloPerson)
+        {
+            return helloPerson.name.ToString() + ", hello!";
+        }
+
+        public ActionResult Hello()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC5Fighting/Sample1_1_1/Models/HelloPerson.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sample1_1_1.Models
+{
+    public class HelloPerson
+    {
+        public String name { get; set; }
+    }
+}
