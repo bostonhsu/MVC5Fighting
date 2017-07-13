40b7d10dd359b3a0a92f497ec0f1c587ccbe68be
Boston Hsu <bostonhsu@outlook.com>
2017-07-13 17:48:39 +0800

    done

--- MVC5Fighting/Sample1-1/App_Start/RouteConfig.cs
+++ MVC5Fighting/Sample1-1/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Sample1_1
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "HelloWorld", action = "Hello", id = UrlParameter.Optional }
             );
         }
     }
---
+++ MVC5Fighting/Sample1-1/Controllers/HelloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sample1_1.Models;
+
+namespace Sample1_1.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string SendHello(HelloPerson helloPerson)
+        {
+            return helloPerson.name.ToString() + ", hello!";
+        }
+
+        public ActionResult Hello()
+        {
+            return View();
+        }
+    }
+}
